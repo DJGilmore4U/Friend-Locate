353f8f0d4b1c3c2af9faa48bff8060776d704ed6
DJGilmore4U <daniellejgilmore@gmail.com>
2018-04-28 10:37:59 -0700

    one

---
+++ app/data/friends.js
@@ -0,0 +1,53 @@
+var friends = [
+	
+	{
+		name: "Lauren Gilmore",
+		photo: "https://scontent-lax3-2.xx.fbcdn.net/v/t1.0-9/16807727_10209594657676045_2185058998470423451_n.jpg?_nc_cat=0&oh=6abe06877a9a8720f3bc253d3fb9dd1d&oe=5B5F473D",
+		scores: [
+			"5",
+			"1",
+			"4",
+			"3",
+			"1",
+			"5",
+			"1",
+			"4",
+			"3",
+			"1"
+		]
+	},
+	{
+		name: "Austin Pepin",
+		photo: "https://scontent-lax3-2.xx.fbcdn.net/v/t1.0-9/936581_10201916428184273_1264361506_n.jpg?_nc_cat=0&oh=06baaa1f6852085c7fc25e9406382c8c&oe=5B5A08DC",
+		scores: [
+			"3",
+			"1",
+			"5",
+			"4",
+			"1",
+			"5",
+			"1",
+			"1",
+			"5",
+			"3"
+		]
+	},
+	{
+		name: "Alex Jones",
+		photo: "https://scontent-lax3-2.xx.fbcdn.net/v/t1.0-9/13895339_105668203212369_8914685938649241603_n.jpg?_nc_cat=0&oh=c528ac7bdc80c9c0d5bac90989affd80&oe=5B6763FE",
+		score: [
+			"3",
+			"1",
+			"4",
+			"1",
+			"4",
+			"3",
+			"5",
+			"1",
+			"5",
+			"4"		
+		]
+	}
+];
+
+module.exports = friends;
